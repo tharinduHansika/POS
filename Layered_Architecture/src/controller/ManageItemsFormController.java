ea0b9c75e3d4d28572d17946ec223d05d709eb34
tharinduHansika <tharindunirma@gmail.com>
2022-05-02 22:47:35 +0530

    Added btnSaveItem code

--- Layered_Architecture/src/controller/ManageItemsFormController.java
+++ Layered_Architecture/src/controller/ManageItemsFormController.java
@@ -187,13 +187,18 @@ public class ManageItemsFormController {
                     new Alert(Alert.AlertType.ERROR, code + " already exists").show();
                 }
                 //Save Item
-                Connection connection = DBConnection.getDbConnection().getConnection();
+                /*Connection connection = DBConnection.getDbConnection().getConnection();
                 PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
                 pstm.setString(1, code);
                 pstm.setString(2, description);
                 pstm.setBigDecimal(3, unitPrice);
                 pstm.setInt(4, qtyOnHand);
-                pstm.executeUpdate();
+                pstm.executeUpdate();*/
+
+                ItemDTO itemDTO = new ItemDTO(code,description,unitPrice,qtyOnHand);
+                ItemDAOImpl itemDAO = new ItemDAOImpl();
+                itemDAO.btnSaveItem(itemDTO);
+
                 tblItems.getItems().add(new ItemTM(code, description, unitPrice, qtyOnHand));
 
             } catch (SQLException e) {
--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -29,4 +29,14 @@ public class ItemDAOImpl {
         pstm.setString(1, code);
         return pstm.executeUpdate()>0;
     }
+
+    public boolean btnSaveItem(ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+        pstm.setString(1, itemDTO.getCode());
+        pstm.setString(2, itemDTO.getDescription());
+        pstm.setBigDecimal(3, itemDTO.getUnitPrice());
+        pstm.setInt(4, itemDTO.getQtyOnHand());
+        return pstm.executeUpdate()>0;
+    }
 }
